8e09e17ed7fa4e7be6506ff2c53e92bb21d44d7d
abdulrahmanalaa123 <abdulrahmanalaa497@gmail.com>
2024-02-10 00:57:30 +0200

    adding axios setup with setting token

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import axiosInterface from "./api/axios";
+
 function App() {
-  return <></>;
+  async function getArticles() {
+    const articles = await axiosInterface.normalInterface.get("/articles");
+    console.log(articles.data);
+  }
+
+  return (
+    <>
+      <div className="bg-blue-500 w-full h-full flex items-center justify-center">
+        <button
+          className="bg-slate-600 rounded-full py-4 px-6 text-3xl hover:bg-red-950 "
+          onClick={() => {
+            getArticles();
+          }}
+        >
+          Auth
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
