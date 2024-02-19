428eba3c1bc64b048f6e8f8718e587683a44ff71
abdulrahmanalaa123 <abdulrahmanalaa497@gmail.com>
2024-02-19 14:23:30 +0200

    adding pagination and good pages handling

--- src/components/Home/articleForm.jsx
+++ src/components/Home/articleForm.jsx
@@ -1,7 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import ArticleCard from "./articleCard";
 import { getArticlesByPage, getYourFeed } from "../../api/articleFetchingApi";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { errorListFormatting } from "../../lib/axios";
 
 function ArticlesForm({ feedState }) {
@@ -10,15 +10,35 @@ function ArticlesForm({ feedState }) {
   const queryFunction =
     feedState === "global" ? getArticlesByPage(page) : getYourFeed(page);
 
-  const { data, isLoading, isError, error } = useQuery({
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    isFetching,
+    isSuccess,
+    isPlaceholderData,
+  } = useQuery({
     queryKey: [feedState, page],
+    placeholderData: keepPreviousData,
     queryFn: () => queryFunction,
   });
+
+  // wanted to put this dependent on articlesCount but couldnt since its conditional on the current data state
+  // and kept giving me an error Rendered more hooks than during the previous render or articlesCount not defined
+  const pagesArray = useMemo(() => {
+    if (isSuccess) {
+      const pagesNo = ~~(data.articlesCount / 20);
+      const pagesArr = [...Array(pagesNo).keys()].map((foo) => foo + 1);
+      return pagesArr;
+    }
+    return [];
+  }, [isSuccess]);
 
   if (isLoading) {
     return (
       <div className="flex justify-center py-12 text-2xl text-accentColor font-bold">
-        Your data is isLoading
+        Data is isLoading
       </div>
     );
   }
@@ -33,6 +53,7 @@ function ArticlesForm({ feedState }) {
       </div>
     );
   }
+
   return (
     <div className="flex flex-col">
       <ArticleCard></ArticleCard>
@@ -40,6 +61,27 @@ function ArticlesForm({ feedState }) {
       <ArticleCard></ArticleCard>
       <ArticleCard></ArticleCard>
       <ArticleCard></ArticleCard>
+      {isFetching ? <span> Loading articles...</span> : null}
+      {isSuccess && (
+        <div className="flex flex-row flex-wrap mt-6 gap-2">
+          {pagesArray.map((pageNo) => {
+            return (
+              <button
+                className={`border-accentColor ${
+                  pageNo - 1 === page ? "bg-accentColor text-white" : ""
+                } hover:underline border-2  px-4 py-1   rounded-md `}
+                onClick={() => {
+                  if (!isPlaceholderData) {
+                    setPage(pageNo - 1);
+                  }
+                }}
+              >
+                {pageNo}
+              </button>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
